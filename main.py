d8e2b9cd90c41e7d7fb556d5bab179daea96e541
Aditi9560 <112256284+Aditi9560@users.noreply.github.com>
2024-08-30 11:11:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,15 +8,14 @@ from openai import OpenAI
 
 recognizer=sr.Recognizer()
 engine = pyttsx3.init()
-newsapi="904676dcac574062bedcb44b4926f058"
+newsapi="newsapikey"
 
 def speak(text):
     engine.say(text)
     engine.runAndWait()
 
 def aiProcess(command):
-    client = OpenAI(api_key="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",)
-
+    client = OpenAI(api_key="open_api_key")
     completion = client.chat.completions.create(
     model="gpt-4o-mini",
     messages=[
